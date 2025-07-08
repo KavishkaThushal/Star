099e6be2d0416241b6dee6748c723dc039a09560
Kavishka Thushal Rasingolla <kavishka.thushal@arimaclanka.com>
2025-07-08 20:22:36 +0530

    FIX:route error

--- src/routes/storeRoute.js
+++ src/routes/storeRoute.js
@@ -1,30 +1,33 @@
 import express from "express";
 import multer from "multer";
 import {
-    createStoreProduct,
-    deleteItem,
-    getAllItems,
-    getAllStores,
-    getStoreById, getStores,
-    updateStoreProduct,
+  createStoreProduct,
+  deleteItem,
+  getAllItems,
+  getAllStores,
+  getStoreById,
+  getStores,
+  updateStoreProduct,
 } from "../controllers/storeController.js";
-import {verifyToken} from "../middleware/verifyToken.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, "uploads/"),
-    filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
 });
 
 const upload = multer({ storage });
 
-router.post("/", upload.single("image"),verifyToken, createStoreProduct);
+router.post("/", upload.single("image"), verifyToken, createStoreProduct);
 router.get("/", getAllItems);
-router.get('/all/stores/products', getStores)
-router.get("/all-store",verifyToken, getAllStores);
-router.put("/:id", upload.single("image"),verifyToken, updateStoreProduct);
-router.get('/:id', getStoreById);
-router.delete("/:id",verifyToken, deleteItem);
+router.get("/all/stores/products", getStores);
+router.get("/all-store", verifyToken, getAllStores);
+
+
+router.get("/:id", getStoreById);
+router.put("/:id", upload.single("image"), verifyToken, updateStoreProduct);
+router.delete("/:id", verifyToken, deleteItem);
 
 export { router as storeRouter };
